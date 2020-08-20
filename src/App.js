67ad8662630c572469f044e1841fd6084b314f11
Mint Project <minttran.9002@gmail.com>
2020-08-20 13:25:55 +0700

    Update component/QuickFacts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Summary from "./components/Summary/Summary";
 import VietnamStats from "./components/VietnamStats/VietnamStats";
 import QuickFacts from './components/QuickFacts/QuickFacts'
 import VietnamQuickFacts from './components/VietnamQuickFacts/VietnamQuickFacts'
+import TabBars from './components/TabBars/TabBars'
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <TabBars/>
         <QuickFacts/>
         <VietnamQuickFacts/>
         <Summary />
