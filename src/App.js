6864b6c1966b7918f10b1e88427dd5a0de8b78ac
Mint Project <minttran.9002@gmail.com>
2020-08-20 16:21:01 +0700

    Merge branch 'master' of https://github.com/miph2004/covid19-stats into Mint

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from "react";
-import Summary from "./components/Summary/Summary";
-import VietnamStats from "./components/VietnamStats/VietnamStats";
-import QuickFacts from './components/QuickFacts/QuickFacts'
-import VietnamQuickFacts from './components/VietnamQuickFacts/VietnamQuickFacts'
-import TabBars from './components/TabBars/TabBars'
 import Header from "./containers/Header/Header";
 class App extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <TabBars/>
-        <QuickFacts/>
-        <VietnamQuickFacts/>
-        <Summary />
-        <VietnamStats />
         <Header />
       </React.Fragment>
     );
