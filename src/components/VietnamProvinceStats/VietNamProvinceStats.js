203c0710ee4f0f18da1daf2fa5c4359a2258a777
huynhhuuy <huynhhuuy1@gmail.com>
2020-08-21 10:04:53 +0700

    Add future/Search/VietNamProvinceStats

--- src/components/VietnamProvinceStats/VietNamProvinceStats.js
+++ src/components/VietnamProvinceStats/VietNamProvinceStats.js
@@ -1,7 +1,8 @@
 import React from "react";
 import axios from "axios";
-import { Table } from "antd";
-
+import { Table, Input, Button, Space } from "antd";
+import { SearchOutlined } from '@ant-design/icons'; 
+import Highlighter from 'react-highlight-words';
 import "./VietnamProvinceStats.css";
 
 class VietnamProvinceStats extends React.Component {
@@ -29,33 +30,102 @@ class VietnamProvinceStats extends React.Component {
         }
       );
   }
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => this.handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex] ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()) : '',
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select(), 100);
+      }
+    },
+    render: text =>
+      this.state.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[this.state.searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+  handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    this.setState({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
 
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: '' });
+  };
   render() {
     const columns = [
       {
         title: "Tỉnh",
         dataIndex: "region",
         key: "region",
+        ...this.getColumnSearchProps('region'),
       },
       {
         title: "Tổng ca nhiễm",
         dataIndex: "totalInfected",
         key: "totalInfected",
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.totalInfected - b.totalInfected,
       },
       {
         title: "Đang điều trị",
         dataIndex: "activeCases",
         key: "activeCases",
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.activeCases - b.activeCases,
       },
       {
         title: "Số ca hồi phục",
         dataIndex: "recovered",
         key: "recovered",
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.recovered - b.recovered,
       },
       {
         title: "Số ca tử vong",
         dataIndex: "deceased",
         key: "deceased",
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.deceased - b.deceased,
       },
     ];
     return (
